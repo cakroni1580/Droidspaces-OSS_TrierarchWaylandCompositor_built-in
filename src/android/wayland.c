77a1be774244a321b18ce695a2ef93ced1932dac
cakroni1580 <cakroni158@gmail.com>
2026-07-19 23:35:59 +0700

    Update wayland.c

--- src/android/wayland.c
+++ src/android/wayland.c
@@ -30,24 +30,6 @@ int ds_setup_wayland_socket(struct ds_config *cfg) {
     return -1;
   }
 
-  /*if (mkdir("/run/droidspaces", 0700) < 0 && errno != EEXIST) {
-    ds_warn("[Wayland] failed to create /run/droidspaces: %s",
-            strerror(errno));
-    return -1;
-  }
-
-  if (chmod("/run/droidspaces", 0700) < 0) {
-    ds_warn("[Wayland] failed to chmod /run/droidspaces: %s",
-            strerror(errno));
-    return -1;
-  }
-
-  if (chown("/run/droidspaces", 0, 0) < 0) {
-    ds_warn("[Wayland] failed to chown /run/droidspaces: %s",
-            strerror(errno));
-    return -1;
-  }*/
-
   /* Stage under /run/droidspaces - immune to user-runtime-dir@0 overmounts.
    * XDG_RUNTIME_DIR and WAYLAND_DISPLAY are set per-user by the rootfs side. */
   if (ds_bind_mount_socket(DS_WL_HOST_SOCKET_OLDROOT, DS_WL_CONTAINER_SOCKET,
